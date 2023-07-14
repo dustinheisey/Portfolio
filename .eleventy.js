c5b7eab0f5df0351da340b0eea23be46b36b551d
Dustin Heisey <dustin@heisey.email>
2023-07-14 12:46:24 -0700

    favicon workflow

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,7 @@
-const common = require('@dh253/nds')
+const common = require("@dh253/nds");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(common, {
-    domain: 'fixme',
-    manifest: {
-      name: 'fixme',
-      short_name: 'fixme',
-      description:
-        'fixme',
-      theme_color: '#fixme',
-      background_color: '#fixme',
-    }
-  })
-}
+    domain: "fixme",
+  });
+};
