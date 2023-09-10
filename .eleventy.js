49b539f0740e909ef790b7f8115a2874d78a1046
Dustin Heisey <dustin@heisey.email>
2023-09-09 19:21:56 -0500

    tweak

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,35 @@ module.exports = function (eleventyConfig) {
     domain: "https://dustinheisey.com",
   });
 
+  eleventyConfig.on("afterBuild", () => {
+    const socialPreviewImagesDir = "_site/img/og/";
+    fs.readdir(socialPreviewImagesDir, (err, files) => {
+      if (files.length > 0) {
+        files.forEach((filename) => {
+          if (filename.endsWith(".svg")) {
+            let imageUrl = socialPreviewImagesDir + filename;
+            Image(imageUrl, {
+              formats: ["jpeg"],
+              outputDir: "./" + socialPreviewImagesDir,
+              filenameFormat: (id, src, width, format, options) => {
+                let outputFilename = filename.substring(0, filename.length - 4);
+                return `${outputFilename}.${format}`;
+              },
+            })
+              .then(() => {
+                fs.unlink(imageUrl, (err) => {
+                  if (err) throw err;
+                });
+              })
+              .catch((error) => {
+                console.error(`Failed to process image: ${error}`);
+              });
+          }
+        });
+      }
+    });
+  });
+
   return {
     dir: {
       layouts: "_layouts",
